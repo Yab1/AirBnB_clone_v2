ae1e795b1d62b4f9f0630ac2e5a5b586063765b1
Yab1 <yabilisanu@gmail.com>
2022-10-27 19:09:24 +0300

    update

--- models/city.py
+++ models/city.py
@@ -1,24 +1,22 @@
-#!/usr/bin/python
-""" holds class City"""
-import models
+#!/usr/bin/python3
+"""This is the city class"""
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """Representation of city """
-    if models.storage_t == "db":
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
-    else:
-        state_id = ""
-        name = ""
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+        places: relationship with place
+        __tablename__: cities table for database
+    """
+    __tablename__ = 'cities'
 
-    def __init__(self, *args, **kwargs):
-        """initializes city"""
-        super().__init__(*args, **kwargs)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship('Place', cascade='all, delete',
+                          backref='cities')
--- models/state.py
+++ models/state.py
@@ -1,34 +1,38 @@
 #!/usr/bin/python3
-""" holds class State"""
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
+"""This is the state class"""
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
+    """This is the class for State
+    Attributes:
+        name: input name
+        __tablename__: name for db table
+        cities: relation with city
+    """
+    __tablename__ = 'states'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship('City', cascade='all, delete',
+                              backref='state')
     else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
         @property
         def cities(self):
-            """getter for list of city instances related to the state"""
+            """Returns the list of City instances with
+            state_id equals to the current State.id
+            city_list: list of cities from a state
+            """
+            from models import storage
+            from models.city import City
+
             city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
+            city_dict = storage.all(City)
+
+            for city in city_dict.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
